fa1bc00ab350193ae95336955406dd030ead754a
Zeyad <zeyadamer285@gmail.com>
2023-08-21 15:45:29 +0300

    Get employee by ID

--- src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDto;
 import net.javaguides.ems.entity.employee;
+import net.javaguides.ems.entity.exception.ResourceNotFoundException;
 import net.javaguides.ems.entity.mapper.EmployeeMapper;
 import net.javaguides.ems.repository.EmployeeRepository;
 import net.javaguides.ems.service.EmployeeService;
@@ -22,5 +23,12 @@ public class EmployeeServiceImpl implements EmployeeService{
         employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
 
+    }
+
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("There is no Employee with this id" + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
